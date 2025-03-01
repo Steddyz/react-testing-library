c07945833ad33f83d8de1f8176011bc67c606b9b
Steddy <qwesteddyoff@gmail.com>
2025-03-01 13:36:11 +0300

    add async + button + input. fireEvent+userEvent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+  const [value, setValue] = useState("");
+
+  const onClick = () => {
+    setToggle((prev) => !prev);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({});
+    }, 1000);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 data-testid="value-element">{value}</h1>
+      {toggle === true && <div data-testid="toggle-element">toggle</div>}
+      {data && <div style={{ color: "red" }}>data</div>}
+
+      <h1>Hello world</h1>
+      <button data-testid="toggle-btn" onClick={onClick}>
+        Click me
+      </button>
+      <input
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="input value"
+      />
     </div>
   );
 }
